d675cffc9a095b18208aa81637e637ffb820c364
austin43 <austin.d.harris@gmail.com>
2024-08-07 16:26:40 -0700

    illustrate [id] being matched higher than create

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, Button, Text } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { Link } from 'expo-router';
 
 export default function HomeScreen() {
   return (
@@ -18,6 +19,12 @@ export default function HomeScreen() {
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
+        <Link href={{ pathname: '/nested/[id]', params: { id: '123' } }} asChild>
+          <Button title="Go to id" />
+        </Link>
+        <Link href={{ pathname: '/nested/create' }} asChild>
+          <Button title="Go to create" />
+        </Link>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
---
+++ app/nested/create/index.tsx
@@ -0,0 +1,5 @@
+import { Text, View } from "react-native";
+
+export default function CreateRoute() {
+  return <Text>{"/nested/create"}</Text>;
+}
